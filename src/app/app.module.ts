ee1f0df328e67b7cd6a606da34f8a907cd8bf32a
Tarcizio Junior <tarcizio.junior2318@outlook.com>
2020-05-22 03:34:28 -0300

    excluindo o component recipe-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipesListComponent } from './recipes-list/recipes-list.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipesListComponent
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
